37dff44e3b00f33e447b12e8101942b3576c833a
gogodigimo <rob.fine@digitsinmotion.com>
2022-03-07 13:04:25 -0500

    Get it to compile

--- build.config.js
+++ build.config.js
@@ -1,7 +1,10 @@
-const webpack = require.main.require('webpack')
-const {name, version} = require('./package.json')
+const webpack = require.main.require('webpack');
+const { name, version } = require('./package.json');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
-const demo = process.env.NODE_ENV === 'development' || process.env.npm_lifecycle_event === 'docs:build'
+const demo =
+  process.env.NODE_ENV === 'development' ||
+  process.env.npm_lifecycle_event === 'docs:build';
 
 module.exports = {
   // generate html only for dev and dist:demo
@@ -14,28 +17,31 @@ module.exports = {
     output: {
       publicPath: '.', // generate client.*.js relative to ./demo/index.html
       filename: demo ? undefined : kebabCase(name) + '.js', // my-component.js
-      library: demo ? undefined : camelCase(name) // MyComponent
+      library: demo ? undefined : camelCase(name), // MyComponent
     },
     plugins: [
+      new UglifyJSPlugin(),
       new webpack.DefinePlugin({
-        'proccess.env.VERSION': JSON.stringify(version) // adds MyComponent.version
-      })
-    ]
-  }
-}
+        'proccess.env.VERSION': JSON.stringify(version), // adds MyComponent.version
+      }),
+    ],
+  },
+};
 
 // utils
 
 // converts MyComponent to my-component
-function kebabCase (s) {
-  return s.replace(/([A-Z])([^A-Z\-])/g, (_, a, b) => `-${a}${b}`)
-          .toLowerCase()
-          .replace(/[\s_-]+/g, '-')
-          .replace(/(^\W)|(\W$)/g, '')
+function kebabCase(s) {
+  return s
+    .replace(/([A-Z])([^A-Z\-])/g, (_, a, b) => `-${a}${b}`)
+    .toLowerCase()
+    .replace(/[\s_-]+/g, '-')
+    .replace(/(^\W)|(\W$)/g, '');
 }
 
 // converts my-component to MyComponent
-function camelCase (s) {
-  return s.replace(/([\-_\s]+[a-z])|(^[a-z])/g, $1 => $1.toUpperCase())
-          .replace(/[\-_\s]+/g, '')
+function camelCase(s) {
+  return s
+    .replace(/([\-_\s]+[a-z])|(^[a-z])/g, ($1) => $1.toUpperCase())
+    .replace(/[\-_\s]+/g, '');
 }
